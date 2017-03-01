6eff6a33060d80783bf23ee61db5c292a0e97ab0
Alex Yang <a.yang96@berkeley.edu>
2017-02-28 23:01:03 -0800

    first commit

--- Hangman/Hangman/HangmanViewController.swift
+++ Hangman/Hangman/HangmanViewController.swift
@@ -9,14 +9,25 @@
 import UIKit
 
 class HangmanViewController: UIViewController {
-
+    var hangmanPhrases: HangmanPhrases?
+    var word: Word?
+    var phrase: String?
+    @IBOutlet weak var StringToGuess: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+        reload()
         
-        let hangmanPhrases = HangmanPhrases()
+    }
+    
+    //start fresh
+    func reload(){
+        hangmanPhrases = HangmanPhrases()
         // Generate a random phrase for the user to guess
-        let phrase: String = hangmanPhrases.getRandomPhrase()
-        print(phrase)
+        phrase = hangmanPhrases!.getRandomPhrase()
+        word = Word(input: phrase!)
+        StringToGuess.text = word!.progressSoFar
+        IncorrectGuesses.text = "Incorrect Guesses:"
+        hangManImage.image = #imageLiteral(resourceName: "hangman1")
     }
 
     override func didReceiveMemoryWarning() {
@@ -25,7 +36,73 @@ class HangmanViewController: UIViewController {
     }
     
 
+
+    @IBOutlet weak var IncorrectGuesses: UILabel!
+
+    @IBOutlet weak var Input: UITextField!
+    
+    @IBAction func EditingChanged(_ sender: UITextField) {
+        if(Input.text!.characters.count>1){
+            let i = Input.text!.index(Input.text!.startIndex, offsetBy: 1)
+            Input.text = String(Input.text![i])
+        }
+        Input.text = Input.text!.uppercased()
+
+        
+    }
+    @IBOutlet weak var hangManImage: UIImageView!
+    @IBAction func Guess(_ sender: UIButton) {
+        
+        //call guess function
+        if(Input.text!.characters.count == 1){
+            word!.guess(input: Input.text!)
+        }
+        
+        //update UIlabels and clear input
+        IncorrectGuesses.text = word!.wrongGuesses
+        StringToGuess.text = word!.progressSoFar
+        Input.text = ""
+        
+        //update image
+        if(word!.numWrongGuesses == 1){
+            hangManImage.image = #imageLiteral(resourceName: "hangman2")
+        } else  if(word!.numWrongGuesses == 2){
+            hangManImage.image = #imageLiteral(resourceName: "hangman3")
+        } else  if(word!.numWrongGuesses == 3){
+            hangManImage.image = #imageLiteral(resourceName: "hangman4")
+        } else  if(word!.numWrongGuesses == 4){
+            hangManImage.image = #imageLiteral(resourceName: "hangman5")
+        } else  if(word!.numWrongGuesses == 5){
+            hangManImage.image = #imageLiteral(resourceName: "hangman6")
+        } else  if(word!.numWrongGuesses == 6){
+            hangManImage.image = #imageLiteral(resourceName: "hangman7")
+        }
+        
+        //check win
+        if(word!.win==1){
+            win()
+        }
+        if(word!.win == 0){
+            lose()
+        }
+    }
+    
+    func lose(){
+        let alert = UIAlertController(title: "You Lost!", message: "Try Again? You have no choice.", preferredStyle: UIAlertControllerStyle.alert)
+        alert.addAction(UIAlertAction(title: "OK!", style: UIAlertActionStyle.default, handler:  { action in
+            self.reload()
+        }))
+        self.present(alert, animated: true, completion: nil)
+    }
+    func win(){
+        let alert = UIAlertController(title: "You Won!", message: "Try Again? You have no choice.", preferredStyle: UIAlertControllerStyle.alert)
+        alert.addAction(UIAlertAction(title: "OK!", style: UIAlertActionStyle.default, handler: { action in
+            self.reload()
+        }))
+        self.present(alert, animated: true, completion: nil)
+    }
     /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
--- Hangman/Word.swift
+++ Hangman/Word.swift
@@ -7,3 +7,67 @@
 //
 
 import Foundation
+
+class Word {
+    let stringToGuess:String
+    var progressSoFar:String
+    var usedCharacters: Array<Character>
+    var wrongGuesses = "Incorrect Guesses:"
+    var numWrongGuesses = 0
+    var win = 2
+    init(input:String) {
+        self.stringToGuess = input
+        self.progressSoFar = ""
+        self.usedCharacters = []
+        for c in input.characters{
+            if(c == " "){
+                self.progressSoFar = self.progressSoFar+" "
+            } else {
+                self.progressSoFar = self.progressSoFar+"-"
+
+            }
+            
+        }
+    }
+    func guess(input:String){
+        var index = 0
+        var guessflag = 0
+        let guess = input.uppercased()[input.startIndex]
+        
+        //quit early if the letter has been guessed already
+        if(usedCharacters.contains(guess)){
+            return
+        } else {
+            usedCharacters.append(guess)
+        }
+        
+        //check if the guess is right and update results
+        var characters = Array(progressSoFar.characters)
+        for c in stringToGuess.characters{
+            if(c == guess){
+                guessflag = 1
+                characters[index] = guess
+            }
+            index += 1
+        }
+        progressSoFar = String(characters)
+        
+        //if guess is wrong
+        if(guessflag == 0){
+            wrongGuesses = wrongGuesses+" "+input
+            numWrongGuesses += 1
+        }
+        
+        //check win
+        if(progressSoFar == stringToGuess){
+            win = 1
+        }
+        //check loss
+        if(numWrongGuesses == 6){
+            win = 0
+        }
+
+        
+    }
+
+}
